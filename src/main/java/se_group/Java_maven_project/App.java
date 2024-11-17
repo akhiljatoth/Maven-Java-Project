c33acfbdc129881983ccad56577a808a58d7b382
king <jatothakhil964@gmanil.com>
2024-11-17 15:53:13 +0530

    javaproject

---
+++ src/main/java/se_group/Java_maven_project/App.java
@@ -0,0 +1,13 @@
+package se_group.Java_maven_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
